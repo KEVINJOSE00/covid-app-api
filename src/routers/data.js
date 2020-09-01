3044e2ce8a2743f6f0fbf788a79a830b315489fc
Kevin Jose <kevinjose199713@gmail.com>
2020-09-01 17:18:11 +0530

    final change :qa
    :qw
    :qa

--- src/routers/data.js
+++ src/routers/data.js
@@ -1,14 +1,16 @@
 const express = require('express')
 
 const Data = require("../models/data")
+const { count } = require('../models/data')
 const router = new express.Router()
 
 
-var risk_status = " "
+
 
 router.post('/data', (req, res ) =>{
     const data = new Data(req.body)
     var total = 0
+    var count = 0
     total  = data.confirmed
     data.count = total  
     var val = data.confirmed - data.recovered
@@ -19,21 +21,31 @@ router.post('/data', (req, res ) =>{
         data.status = "High risk"
          }else if (Math.abs(val)>100) {
             data.status ="High risk"
+        
+         }
+       
+        
+         var adjacent_district_string = data.adjacent_district
+         //console.log(adjacent_district_string)
+        var adjacent_district = adjacent_district_string.split(" ")
+        
+         length = adjacent_district.length
+        
+        for(i=0; i<length; i++)
+        
+        {
+             Data.find({district_id :adjacent_district[i]}).then((adj_data) =>{
+                 if(adj_data[0].status=="High risk"){
+                     count = count + 1
+                     if(count > ((count/100)*70)){
+                         data.status = "Extereme risk region"
+                     }
+                  }
+                
+            })
+            
         }
-    
-   
-//    var adjacent_district_string = data.adjacent_district
-//     var adjacent_district = adjacent_district_string.split(" ")
-//     console.log(adjacent_district)
-//     length = adjacent_district.length
-   
-//     for (i=0; i<length; i++)
-//     {
-//     Data.find({district_id :adjacent_district[i]}).then((adj_data) =>{
-//         console.log(adj_data)
-//     })
-//    }
-    
+        
     data.save().then(() =>{
        res.send(data)
 
@@ -54,23 +66,7 @@ router.patch('/data/:id', async (req, res) =>{
         }
 
         res.send(data)
-        var adjacent_district_string = data.adjacent_district
-        var adjacent_district = adjacent_district_string.split(" ")
         
-        length = adjacent_district.length
-        
-        for(i=0; i<length; i++)
-
-        {
-        Data.find({district_id :adjacent_district[i]}).then((adj_data) =>{
-                  console.log(adj_data)
-         })
-    
-        }
-
-
-
-
     } catch(e){
         res.status(400).send(e)
     }
